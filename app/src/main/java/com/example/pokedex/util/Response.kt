17b8c2a4b74157972ff1bf4ca323384261592ba6
ceasarpimentel <ceasar.pimentel@gmail.com>
2023-01-14 12:50:50 -0800

    made data nullable on success and gave it a default value

--- app/src/main/java/com/example/pokedex/util/Response.kt
+++ app/src/main/java/com/example/pokedex/util/Response.kt
@@ -1,7 +1,7 @@
 package com.example.pokedex.util
 
 sealed class Response<T>(val data:T? = null, val message: String? = null) {
-    class Success<T>(data: T?): Response<T>(data)
+    class Success<T>(data: T?= null): Response<T>(data)
     class Error<T>(message: String, data:T?= null): Response<T>(message = message, data = data)
     class Loading<T>(data: T? = null): Response<T>(data)
 }
