472a23fe11dfc9619f18cbeadbcee62ce69c4f9b
ceasarpimentel <ceasar.pimentel@gmail.com>
2023-01-14 12:45:50 -0800

    implemented methods for retrofit api service
    Implemented get pokemons and  get pokemon

--- app/src/main/java/com/example/pokedex/data/network/PokedexApiService.kt
+++ app/src/main/java/com/example/pokedex/data/network/PokedexApiService.kt
@@ -1,11 +1,17 @@
 package com.example.pokedex.data.network
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
+import com.example.pokedex.data.network.responses.Pokemon
+import com.example.pokedex.data.network.responses.PokemonList
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface PokedexApiService {
+
+    @GET("pokemon")
+    suspend fun getPokemons(@Query("limit")limit: Int, @Query("offset")offset: Int) : PokemonList
+
+    @GET("pokemon/{name}")
+    suspend fun getPokemon(@Path("name")name: String) : Pokemon
 }
 
